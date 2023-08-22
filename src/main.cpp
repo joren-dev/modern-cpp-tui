c6ef997b90d4a13331e393eaa9de5ca676c2d313
joren-dev <contactjorenx@gmail.com>
2023-08-23 01:37:34 +0200

    add: GLM as dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 ﻿#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 // Testing purposes
-#include <vulkan/vulkan.hpp>
 #include "test.hpp"
 
 #include <iostream>
@@ -20,6 +24,10 @@ int main( )
     vkEnumerateInstanceExtensionProperties( nullptr, &extensionCount, nullptr );
 
     std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    const auto test = matrix * vec;
 
     while ( !glfwWindowShouldClose( window ) )
     {
