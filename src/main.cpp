be14767d4899071e0084e9f2c2acc4710d0a500f
joren-dev <contactjorenx@gmail.com>
2023-08-23 00:38:57 +0200

    add: glfw to linking

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
 ﻿#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/mat4x4.hpp>
-#include <glm/vec4.hpp>
 
 #include <iostream>
 
@@ -19,10 +15,6 @@ int main( )
     vkEnumerateInstanceExtensionProperties( nullptr, &extensionCount, nullptr );
 
     std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
 
     while ( !glfwWindowShouldClose( window ) )
     {
